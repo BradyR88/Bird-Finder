9c868e883962a8d00f722626b0538655a2da86c9
Brady Robshaw <heybrady88@gmail.com>
2022-10-25 12:26:38 -0400

    Initial Commit

---
+++ Bird Finder/Bird_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bird_FinderApp.swift
+//  Bird Finder
+//
+//  Created by Brady Robshaw on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Bird_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bird Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bird Finder
+//
+//  Created by Brady Robshaw on 10/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
