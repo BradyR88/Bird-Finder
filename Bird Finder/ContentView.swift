d688ab7b0cf51fc7cd3097966686a614e1d36bd0
Brady Robshaw <heybrady88@gmail.com>
2022-11-07 13:14:46 -0500

    - move logic out view and in to a viewModel

--- Bird Finder/ContentView.swift
+++ Bird Finder/ContentView.swift
@@ -11,60 +11,30 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var viewModel: ViewModel
     
-    @StateObject var deviceLocationService = DeviceLocationService.shared
-
-    @State var tokens: Set<AnyCancellable> = []
-    @State var coordinates: (lat: Double, lon: Double) = (0, 0) {
-        didSet {
-            Task {
-                await viewModel.gitHotSpots(lat: coordinates.lat, lng: coordinates.lon)
-            }
-        }
-    }
-    
     var body: some View {
         VStack {
-            Text("Latitude: \(coordinates.lat)")
-                .font(.largeTitle)
-            Text("Longitude: \(coordinates.lon)")
-                .font(.largeTitle)
+//            Text("Latitude: \(coordinates.lat)")
+//                .font(.largeTitle)
+//            Text("Longitude: \(coordinates.lon)")
+//                .font(.largeTitle)
             
             List(viewModel.hotSpots, id: \.locId) {hotSpot in
                 Text(hotSpot.locId)
             }
             
             Button {
-                deviceLocationService.requestLocationUpdates()
+                viewModel.observeCoordinateUpdates()
+                viewModel.location.requestLocationUpdates()
             } label: {
                 Text("update")
             }
 
         }
         .onAppear {
-            observeCoordinateUpdates()
-            observeDeniedLocationAccess()
-            deviceLocationService.requestLocationUpdates()
+            viewModel.observeCoordinateUpdates()
+            viewModel.observeDeniedLocationAccess()
+            viewModel.location.requestLocationUpdates()
         }
-    }
-    
-    func observeCoordinateUpdates() {
-        deviceLocationService.coordinatesPublisher
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                print("Handle \(completion) for error and finished subscription.")
-            } receiveValue: { coordinates in
-                self.coordinates = (coordinates.latitude, coordinates.longitude)
-            }
-            .store(in: &tokens)
-    }
-
-    func observeDeniedLocationAccess() {
-        deviceLocationService.deniedLocationAccessPublisher
-            .receive(on: DispatchQueue.main)
-            .sink {
-                print("Handle access denied event, possibly with an alert.")
-            }
-            .store(in: &tokens)
     }
 }
 
--- Bird Finder/ViewModel/ViewModel.swift
+++ Bird Finder/ViewModel/ViewModel.swift
@@ -6,12 +6,22 @@
 //
 
 import Foundation
+import Combine
 
 class ViewModel: ObservableObject {
     
     @Published var hotSpots: [HotSpot] = []
     
-    var location = DeviceLocationService.shared
+    var tokens: Set<AnyCancellable> = []
+    var coordinates: (lat: Double, lon: Double) = (0, 0) {
+        didSet {
+            Task {
+                await gitHotSpots(lat: coordinates.lat, lng: coordinates.lon)
+            }
+        }
+    }
+    
+    let location = DeviceLocationService.shared
     
     func gitHotSpots(lat: Double, lng: Double)async {
         let git = APIGiter(gitType: .NearbyHotspots)
@@ -19,9 +29,30 @@ class ViewModel: ObservableObject {
             let data:[HotSpot] = try await git.data(lat: lat, lng: lng)
             DispatchQueue.main.async {
                 self.hotSpots = data
+                print("new spots")
             }
         } catch {
             print("Could not fetch hotspot data \(error.localizedDescription)")
         }
+    }
+
+    func observeCoordinateUpdates() {
+        location.coordinatesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                print("Handle \(completion) for error and finished subscription.")
+            } receiveValue: { coordinates in
+                self.coordinates = (coordinates.latitude, coordinates.longitude)
+            }
+            .store(in: &tokens)
+    }
+
+    func observeDeniedLocationAccess() {
+        location.deniedLocationAccessPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {
+                print("Handle access denied event, possibly with an alert.")
+            }
+            .store(in: &tokens)
     }
 }
